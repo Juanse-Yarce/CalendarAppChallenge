a796e49516d07076d2bfd7769ba549d634b51b7e
Juanse-Yarce <jlopezyarce@gmail.com>
2024-09-12 12:55:57 -0500

    I add the method add_reminder

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -28,7 +28,11 @@ class Event:
     reminders: List[Reminder] = field(default_factory=list)
     id: str = field(default_factory=generate_unique_id)
     
+    def add_reminder(self, date_time: datetime, type_: str = Reminder.EMAIL):
+        reminder = Reminder(date_time=date_time, type=type_)
+        self.reminders.append(reminder)
     
+   
 
 
 # TODO: Implement Day class here
