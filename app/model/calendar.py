a9f4fc8df465ac43899f290ffba1310826f1c525
Juanse-Yarce <jlopezyarce@gmail.com>
2025-04-03 05:18:51 -0500

    cree la clase day con su constructor

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -39,3 +39,8 @@ class Event:
     def __str__(self):
         return f"ID: {self.id}\nEvent title: {self.title}\nDescription: {self.description}\nTime: {self.start_at} - {self.end_at}"
 
+class Day:
+    def __init__(self, date_: date):
+        self.date_ = date_
+        self.slots: dict[time, str | None] = {}
+        self._init_slots()
